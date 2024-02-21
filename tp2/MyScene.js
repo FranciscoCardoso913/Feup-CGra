b67ff99c24cd89f78cb42a0d93bbb08be7fb992f
Antonio Azevedo <up202108689@edu.fe.up.pt>
2024-02-21 10:28:09 +0000

    exercise 2 done

--- tp2/MyScene.js
+++ tp2/MyScene.js
@@ -6,117 +6,144 @@ import { MyTriangleSmall } from "./MyTriangleSmall.js";
 import { MyTriangleBig } from "./MyTriangleBig.js";
 
 /**
- * MyScene
- * @constructor
- */
-export class MyScene extends CGFscene {
-  constructor() {
-    super();
-  }
-  init(application) {
-    super.init(application);
-    
-    this.initCameras();
-    this.initLights();
+    * MyScene
+    * @constructor
+    */
+    export class MyScene extends CGFscene {
+        constructor() {
+            super();
+        }
+        init(application) {
+            super.init(application);
 
-    //Background color
-    this.gl.clearColor(0.0, 0.0, 0.0, 1.0);
+            this.initCameras();
+            this.initLights();
 
-    this.gl.clearDepth(100.0);
-    this.gl.enable(this.gl.DEPTH_TEST);
-    this.gl.enable(this.gl.CULL_FACE);
-    this.gl.depthFunc(this.gl.LEQUAL);
+            //Background color
+            this.gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
-    //Initialize scene objects
-    this.axis = new CGFaxis(this);
-    this.diamond = new MyDiamond(this);
-    this.triangle = new MyTriangle(this);
-    this.parallelogram = new MyParallelogram(this);
-    this.triangleBig = new MyTriangleBig(this);
-    this.triangleSmall = new MyTriangleSmall(this);
+            this.gl.clearDepth(100.0);
+            this.gl.enable(this.gl.DEPTH_TEST);
+            this.gl.enable(this.gl.CULL_FACE);
+            this.gl.depthFunc(this.gl.LEQUAL);
 
-    //Objects connected to MyInterface
-    this.displayAxis = true;
-    this.displayDiamond = true;
-    this.displayTriangle = true;
-    this.displayParallelogram = true;
-    this.displayTriangleSmall = true;
-    this.displayTriangleBig = true;
-    this.scaleFactor = 1;
-  }
-  initLights() {
-    this.lights[0].setPosition(15, 2, 5, 1);
-    this.lights[0].setDiffuse(1.0, 1.0, 1.0, 1.0);
-    this.lights[0].enable();
-    this.lights[0].update();
-  }
-  initCameras() {
-    this.camera = new CGFcamera(
-      0.4,
-      0.1,
-      500,
-      vec3.fromValues(15, 15, 15),
-      vec3.fromValues(0, 0, 0)
-    );
-  }
-  setDefaultAppearance() {
-    this.setAmbient(0.2, 0.4, 0.8, 1.0);
-    this.setDiffuse(0.2, 0.4, 0.8, 1.0);
-    this.setSpecular(0.2, 0.4, 0.8, 1.0);
-    this.setShininess(10.0);
-  }
-  display() {
-    // ---- BEGIN Background, camera and axis setup
-    // Clear image and depth buffer everytime we update the scene
-    this.gl.viewport(0, 0, this.gl.canvas.width, this.gl.canvas.height);
-    this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
-    // Initialize Model-View matrix as identity (no transformation
-    this.updateProjectionMatrix();
-    this.loadIdentity();
-    // Apply transformations corresponding to the camera position relative to the origin
-    this.applyViewMatrix();
+            //Initialize scene objects
+            this.axis = new CGFaxis(this);
+            this.diamond = new MyDiamond(this);
+            this.triangle = new MyTriangle(this);
+            this.parallelogram = new MyParallelogram(this);
+            this.triangleBig = new MyTriangleBig(this);
+            this.triangleBig2 = new MyTriangleBig(this);
+            this.triangleSmall = new MyTriangleSmall(this);
+            this.triangleSmall2 = new MyTriangleSmall(this);
 
-    // Draw axis
-    if (this.displayAxis) this.axis.display();
+            //Objects connected to MyInterface
+            this.displayAxis = true;
+            this.displayDiamond = true;
+            this.displayTriangle = true;
+            this.displayParallelogram = true;
+            this.displayTriangleSmall = true;
+            this.displayTriangleBig = true;
+            this.scaleFactor = 1;
+        }
+        initLights() {
+            this.lights[0].setPosition(15, 2, 5, 1);
+            this.lights[0].setDiffuse(1.0, 1.0, 1.0, 1.0);
+            this.lights[0].enable();
+            this.lights[0].update();
+        }
+        initCameras() {
+            this.camera = new CGFcamera(
+                0.4,
+                0.1,
+                500,
+                vec3.fromValues(15, 15, 15),
+                vec3.fromValues(0, 0, 0)
+            );
+        }
+        setDefaultAppearance() {
+            this.setAmbient(0.2, 0.4, 0.8, 1.0);
+            this.setDiffuse(0.2, 0.4, 0.8, 1.0);
+            this.setSpecular(0.2, 0.4, 0.8, 1.0);
+            this.setShininess(10.0);
+        }
+        display() {
+            // ---- BEGIN Background, camera and axis setup
+            // Clear image and depth buffer everytime we update the scene
+            this.gl.viewport(0, 0, this.gl.canvas.width, this.gl.canvas.height);
+            this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
+            // Initialize Model-View matrix as identity (no transformation
+                this.updateProjectionMatrix();
+                this.loadIdentity();
+                // Apply transformations corresponding to the camera position relative to the origin
+                this.applyViewMatrix();
 
-    this.setDefaultAppearance();
+                // Draw axis
+                if (this.displayAxis) this.axis.display();
 
-    var sca = [
-      this.scaleFactor,
-      0.0,
-      0.0,
-      0.0,
-      0.0,
-      this.scaleFactor,
-      0.0,
-      0.0,
-      0.0,
-      0.0,
-      this.scaleFactor,
-      0.0,
-      0.0,
-      0.0,
-      0.0,
-      1.0,
-    ];
+                this.setDefaultAppearance();
 
-    this.multMatrix(sca);
-    this.pushMatrix();
-    // ---- BEGIN Primitive drawing section
-    this.multMatrix([
-        1,0,0,0,
-        0,1,0,0,
-        0,0,1,0,
-        -1,1,0,1
-    ]);
-    if (this.displayDiamond) this.diamond.display();
-    this.popMatrix();
-    if (this.displayTriangle) this.triangle.display();
-    if (this.displayParallelogram) this.parallelogram.display();
-    if (this.displayTriangleBig) this.triangleBig.display();
-    if (this.displayTriangleSmall) this.triangleSmall.display();
-   
+                var sca = [
+                    this.scaleFactor,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    this.scaleFactor,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    this.scaleFactor,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    1.0,
+                ];
 
-    // ---- END Primitive drawing section
-  }
-}
+            this.multMatrix(sca);
+            this.pushMatrix();
+            // ---- BEGIN Primitive drawing section
+            this.multMatrix([
+                1,0,0,0,
+                0,1,0,0,
+                0,0,1,0,
+                -1,1,0,1
+            ]);
+            if (this.displayDiamond) this.diamond.display();
+            this.popMatrix();
+            this.pushMatrix();
+            this.translate(1,0,0);
+            this.rotate(Math.PI, 0, 0, 1);
+            if (this.displayTriangle) this.triangle.display();
+            this.popMatrix();
+            this.pushMatrix();
+            this.translate(-1,2,0);
+            this.scale(1,-1,1);
+            if (this.displayParallelogram) this.parallelogram.display();
+            this.popMatrix();
+            this.pushMatrix();
+            this.translate(0,-1,0);
+            if (this.displayTriangleBig) this.triangleBig.display();
+            this.popMatrix();
+            this.pushMatrix();
+            this.translate(-2,0,0);
+            this.rotate(-Math.PI / 2,0,0,1); 
+            if (this.displayTriangleSmall) this.triangleSmall.display();
+            this.popMatrix();
+            this.pushMatrix();
+            this.translate(0,-1,0);
+            this.scale(1,-1,1);
+            this.triangleBig2.display();
+            this.popMatrix();
+            this.pushMatrix();
+            this.translate(0,2,0);
+            this.rotate(-Math.PI/4, 0,0,1);
+            this.translate(-1,0,0);
+            this.triangleSmall2.display();
+
+
+            // ---- END Primitive drawing section
+        }
+    }
