f1e210ead4e631b918c93db8292d5450740f178a
Francisco Cardoso <franciscocardoso.3003@gmail.com>
2024-04-10 10:30:55 +0100

    Defining the sphere vertices and indexes

---
+++ project/objects/sphere/MySphere.js
@@ -0,0 +1,93 @@
+import {CGFobject} from '../../../lib/CGF.js';
+
+export class MySphere extends CGFobject {
+	constructor(scene, stacks, slices) {
+		super(scene);
+		this.stacks = stacks;
+        this.slices = slices;
+		this.initBuffers();
+	}
+	initBuffers() {
+		// Generate vertices, normals, and texCoords
+		this.vertices = [];
+		//this.normals = [];
+		//this.texCoords = [];
+		const beta = (Math.PI*2) / this.slices; 
+        const alfa = (Math.PI/2) / this.stacks;
+        let ang2 =0
+        for (let i=0; i < this.slices;i++){
+            let height = Math.sin(ang2);
+            let radius = Math.cos(ang2);
+            let ang = 0;
+            for(let j =0; j< this.stacks; j++){
+                this.vertices.push(...[radius*Math.cos(ang),height,radius*Math.sin(ang)]);
+                ang += beta;
+            }
+            ang2 += alfa;
+        }
+		ang2 = 0;
+        for (let i=0; i < this.slices;i++){
+            let height = Math.sin(ang2);
+            let radius = Math.cos(ang2);
+            let ang = 0;
+            for(let j =0; j< this.stacks; j++){
+                this.vertices.push(...[radius*Math.cos(ang),height,radius*Math.sin(ang)]);
+                ang += beta
+            }
+            ang2 -= alfa;
+        }
+
+        console.log(this.vertices);
+
+
+
+		
+		// Generating indices
+		this.indices = [];
+
+		for (let i=0; i < this.stacks;i++){
+
+			for (let j=0; j < this.slices;j++){
+				this.indices.push(i*this.slices + j);
+				this.indices.push(i*this.slices + ((j+1)%this.slices));
+				this.indices.push(((i+1)%this.stacks)*this.slices + j);
+				
+				this.indices.push(i*this.slices + ((j+1)%this.slices));
+				this.indices.push(((i+1)%this.stacks)*this.slices + j);
+				this.indices.push(((i+1)%this.stacks)*this.slices + ((j+1)%this.slices));
+				
+			}
+		}
+
+		console.log(this.indices);
+
+
+/*
+		var ind = 0;
+		for (var j = 0; j < this.nrDivs; j++) {
+			for (var i = 0; i <= this.nrDivs; i++) {
+				this.indices.push(ind);
+				this.indices.push(ind + this.nrDivs + 1);
+				ind++;
+			}
+			if (j + 1 < this.nrDivs) {
+				this.indices.push(ind + this.nrDivs);
+				this.indices.push(ind);
+			}
+		}*/
+		this.primitiveType = this.scene.gl.TRIANGLE_STRIP;
+		this.initGLBuffers();
+	};
+
+	setFillMode() { 
+		this.primitiveType=this.scene.gl.TRIANGLE_STRIP;
+	};
+
+	setLineMode() 
+	{ 
+		this.primitiveType=this.scene.gl.LINES;
+	};
+
+}
+
+
